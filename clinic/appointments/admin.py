cc7cc740460ec9280cf83a99c82455e6eaf21da4
Kopal22 <45765669+Kopal22@users.noreply.github.com>
2021-09-09 20:00:47 +0530

    Delete clinic directory

--- clinic/appointments/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . import models
-# Register your models here.
-
-admin.site.register(models.Doctor)
-admin.site.register(models.Patient)
-admin.site.register(models.Slot)
-admin.site.register(models.Appointment)
